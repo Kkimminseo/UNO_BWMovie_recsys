b38a4117b3873cff0c7c259d678e3a13649eb8a3
배민경 <bmk0703@naver.com>
2025-02-15 20:32:46 +0900

    fix : '', -> 'signup/'

--- DRF-Backend/accounts/urls.py
+++ DRF-Backend/accounts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.SignUpView.as_view(), name="signup"), # 회원 가입
+    path('signup/', views.SignUpView.as_view(), name="signup"), # 회원 가입
 ]
