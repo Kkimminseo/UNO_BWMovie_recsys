f5fae1a6574edea349e605fa93642fa2ee01267a
canit0221 <rladydtn78@gmail.com>
2025-02-20 17:23:42 +0900

    add: front

---
+++ react-frontend/src/api/axios.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:8000/api/v1';
+
+const axiosInstance = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// 요청 인터셉터 - 토큰이 필요한 요청에 자동으로 토큰 추가
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('access_token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance; 
