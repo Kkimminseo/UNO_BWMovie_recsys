6e6d24215616bbf131770bd602a555834f569a26
배민경 <bmk0703@naver.com>
2025-02-21 23:07:59 +0900

    fix : movielist url

--- DRF-Backend/movies/urls.py
+++ DRF-Backend/movies/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/movielist/', views.SignUpMovieListView.as_view()), # Signup movie list로 가기
+    path('movielist/', views.SignUpMovieListView), # Signup movie list로 가기
 ]
