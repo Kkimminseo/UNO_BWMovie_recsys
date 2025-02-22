4a5c6b103e5d32bf46f127fc617df0e2b90606ed
배민경 <bmk0703@naver.com>
2025-02-22 15:24:39 +0900

    fix : SignupMovieListSerializer에 genres field 추가

--- DRF-Backend/movies/serializers.py
+++ DRF-Backend/movies/serializers.py
@@ -6,11 +6,16 @@ class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
         fields = ['id', 'genre']
-        read_only_fields = ['id']
+        
+        """따로 수정할 게 아니라면 읽기 모드로 넣어서 데이터 보호"""
+        read_only_fields = ['id', 'genre']
 
-"""df와 drf를 직렬화"""
+
+"""회원가입 시, 보여지는 영화 포스터 리스트 및 장르"""
 class SignupMovieListSerializer(serializers.ModelSerializer):
+    genres = GenreSerializer(many=True, read_only=True)
+    
     class Meta:
         model = Movie
-        fields = ['id', 'original_title', 'poster_path']
+        fields = ['id', 'original_title', 'poster_path', 'genres']
         read_only_fields = ['id']
