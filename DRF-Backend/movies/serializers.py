01efa0241a1d83b117f01be36993a99e9846ecb7
배민경 <bmk0703@naver.com>
2025-02-22 14:45:16 +0900

    add : read_only_field

--- DRF-Backend/movies/serializers.py
+++ DRF-Backend/movies/serializers.py
@@ -1,5 +1,12 @@
 from rest_framework import serializers
-from .models import Movie
+from .models import Movie, Genre
+
+"""GenreSerializer"""
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ['id', 'genre']
+        read_only_fields = ['id']
 
 """df와 drf를 직렬화"""
 class SignupMovieListSerializer(serializers.ModelSerializer):
