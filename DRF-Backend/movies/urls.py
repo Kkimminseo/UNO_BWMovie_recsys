e86319131d795d642664f5a14ffb6c8b3ffdb8df
배민경 <bmk0703@naver.com>
2025-02-22 17:37:18 +0900

    add : genrepreference url

--- DRF-Backend/movies/urls.py
+++ DRF-Backend/movies/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('movielist/', views.SignUpMovieListView), # Signup movie list로 가기
+    path('genrepreference/<int:genre_id>/', views.CreateGenrePreferenceView) # 선호 장르 생성
 ]
