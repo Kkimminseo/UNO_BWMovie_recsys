1cdca14ed81cf5538a5d65530c8dc9d61c965aef
배민경 <bmk0703@naver.com>
2025-02-23 21:39:55 +0900

    fix : GenreSerializer 지움

--- DRF-Backend/movies/serializers.py
+++ DRF-Backend/movies/serializers.py
@@ -1,16 +1,6 @@
 from rest_framework import serializers
 from .models import Movie, Genre, MoviePreference
 
-"""GenreSerializer"""
-class GenreSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Genre
-        fields = ['id', 'genre']
-        
-        """따로 수정할 게 아니라면 읽기 모드로 넣어서 데이터 보호"""
-        read_only_fields = ['id', 'genre']
-
-
 """MovieSerializer"""
 class MoviePreferenceSerializer(serializers.ModelSerializer):
     class Meta:
@@ -39,9 +29,8 @@ class MoviePreferenceSerializer(serializers.ModelSerializer):
 
 """회원가입 시, 보여지는 영화 포스터 리스트 및 장르"""
 class SignupMovieListSerializer(serializers.ModelSerializer):
-    genres = GenreSerializer(many=True, read_only=True)
     
     class Meta:
         model = Movie
-        fields = ['id', 'original_title', 'poster_path', 'genres']
+        fields = ['id', 'original_title', 'poster_path']
         read_only_fields = ['id']
