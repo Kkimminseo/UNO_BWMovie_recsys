ebf9e26eca67ba904f648d13a649f01c2d1747d6
배민경 <bmk0703@naver.com>
2025-02-23 19:51:47 +0900

    fix : moviepreference 오류 해결

--- DRF-Backend/movies/views.py
+++ DRF-Backend/movies/views.py
@@ -41,18 +41,18 @@ class CreateMoviePreferenceView(generics.CreateAPIView):
     serializer_class = MoviePreferenceSerializer
 
     def post(self, request, *args, **kwargs):
-        print(self.__class__.__name__+" post()")
+        # print(self.__class__.__name__+" post()")
         """
         사용자가 선택한 영화를 받아서, 영화 선호도 데이터베이스에 저장
         """
-        print("request.data : ", request.data)
+        # print("request.data : ", request.data)
         selected_movie_ids = request.data.get('movie_id_fk', [])
-        print(type(selected_movie_ids))
-        print("selected_movie_ids: ", selected_movie_ids)
+        # print(type(selected_movie_ids))
+        # print("selected_movie_ids: ", selected_movie_ids)
         # 예외 처리: 선택한 영화 개수 제한
         if type(selected_movie_ids) == str:
             selected_movie_ids = [selected_movie_ids]
-        print(type(selected_movie_ids))
+        # print(type(selected_movie_ids))
         if len(selected_movie_ids) > 5:
             return Response({"error": "선호 영화는 최대 5개까지만 선택할 수 있습니다."},
                             status=status.HTTP_400_BAD_REQUEST)
@@ -61,16 +61,18 @@ class CreateMoviePreferenceView(generics.CreateAPIView):
 
         # 선호 영화 ID를 순회하며 MoviePreference 객체 생성
         for movie_id in selected_movie_ids:
-            print("movie_id: ", movie_id)
+            # print("movie_id: ", movie_id)
+            # print(type(movie_id))
             try:
-                movie = Movie.objects.get(pk=movie_id)  # 영화 ID로 Movie 객체 조회
+                movie = Movie.objects.get(id=movie_id)  # 영화 ID로 Movie 객체 조회
+                # print("movie: ", movie)
                 movie_preference = MoviePreference.objects.create(
                     user_id_fk=user,
                     movie_id_fk=movie,
                     preference_type='like'  # 또는 'dislike' 등, 필요에 따라 변경
                 )
                 movie_preference.save()
-                print("movie: ", movie)
+                # print("movie: ", movie)
             except Movie.DoesNotExist:
                 return Response({"error": f"ID '{movie_id}'에 해당하는 영화가 존재하지 않습니다."},
                                 status=status.HTTP_400_BAD_REQUEST)
