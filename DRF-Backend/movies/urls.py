afb8ada689960ca897398fa6f92bcd7c6657fb66
배민경 <bmk0703@naver.com>
2025-02-23 18:24:50 +0900

    add : moviepreference url

--- DRF-Backend/movies/urls.py
+++ DRF-Backend/movies/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('movielist/', views.SignUpMovieListView.as_view()), # Signup movie list로 가기
-    path('genrepreference/', views.CreateGenrePreferenceView.as_view()), # 선호 장르 생성
+    # path('genrepreference/', views.CreateGenrePreferenceView.as_view()), # 선호 장르 생성
+    path('moviepreference/', views.CreateMoviePreferenceView.as_view()), # 선호 영화 생성
 ]
